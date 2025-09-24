df5b60902df59a9464f2481448e88261013ec747
Vitor Campos <vitor.campos@spassu.com.br>
2025-09-24 10:39:55 -0300

    chore: Adição do prefixo do ms na url do schema do swagger

--- config/urls.py
+++ config/urls.py
@@ -13,7 +13,7 @@ def healthcheck(_request):
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('candidatos.urls')),
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('ms-candidatos/api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('', healthcheck, name='healthcheck'),
 ]
