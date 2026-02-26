49134749f88627b5529bff3a1dc21bbf7411d2c7
eudesgomesss <developer.eudes@gmail.com>
2026-02-26 13:54:42 -0300

    test(137123): Adiciona testes unitários para reclassificados e corrige testes unitários quebrados

--- candidatos/tests/views/test_candidatos.py
+++ candidatos/tests/views/test_candidatos.py
@@ -39,7 +39,8 @@ def test_create_candidatos_em_lote(api_client, candidato_url):
     assert ConcursoCandidatosLote.objects.count() == 1
     lote = ConcursoCandidatosLote.objects.first()
     assert ConcursoCandidato.objects.filter(lote=lote).count() == 1
-    assert Candidato.objects.filter(cpf='555.666.777-88').exists()
+    # CPF é armazenado sem máscara pelo serviço
+    assert Candidato.objects.filter(cpf='55566677788').exists()
 
 
 def test_retrieve_candidato(api_client, candidatos_criados):
